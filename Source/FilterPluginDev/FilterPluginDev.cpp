939c5443780bf5c5fe40077bcba7d71eadd86d72
motiondj <motiondj@naver.com>
2025-05-25 20:19:46 +0900

    Initial commit

---
+++ Source/FilterPluginDev/FilterPluginDev.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FilterPluginDev.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FilterPluginDev, "FilterPluginDev" );
